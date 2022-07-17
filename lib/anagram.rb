a37f8bc0b6ecce72c724b814a576ad01ce825353
Austin <baik.austin@gmail.com>
2022-07-17 12:12:19 -0700

    commit tree 99e3a8ef9099861e013c899f729d672128b54e39

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+
+class Anagram 
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word 
+    end
+
+    def match(array)
+        array.select {|x| x.split("").sort == @word.split("").sort} 
+    end
+        
+end
+
+
+        #take each item in array, and #chars into array 
+        #then #sort and check using {==}
+       
+    
